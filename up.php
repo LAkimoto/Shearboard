9598dbcd242d5da35da991e067ae745eb1e01181
LAkimoto <2428316292@qq.com>
2023-05-27 22:05:06 +0800

    同时修改php，使其支持markdown

--- up.php
+++ up.php
@@ -1,14 +1,28 @@
 <?php
-header('Content-Type: application/json');
-
+// Handle the POST request for saving the content
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $data = json_decode(file_get_contents('php://input'), true);
-  $content = $data['content'];
-  $timestamp = $data['timestamp'];
-  file_put_contents('data.txt', $content);
-  echo json_encode(['status' => 'success']);
-} else {
-  $content = file_get_contents('data.txt');
-  echo json_encode(['content' => $content]);
+    $content = $_POST['content'];
+
+    // Save the content to a file or database
+    // Example: Saving to a file named "saved_content.txt"
+    file_put_contents('saved_content.txt', $content);
+
+    // Send a response if needed
+    // Example: Sending a JSON response
+    $response = array('status' => 'success', 'message' => 'Content saved successfully');
+    echo json_encode($response);
+    exit;
+}
+
+// Handle the GET request for retrieving the content
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    // Retrieve the content from a file or database
+    // Example: Retrieving from a file named "saved_content.txt"
+    $content = file_get_contents('saved_content.txt');
+
+    // Send the content as a JSON response
+    $response = array('content' => $content);
+    echo json_encode($response);
+    exit;
 }
 ?>
